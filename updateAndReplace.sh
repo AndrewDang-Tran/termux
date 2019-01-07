1d14e728e4911292c4d56af06308255472375e76
Andrew Dang-Tran <andrewdt1506@gmail.com>
2019-01-06 16:20:25 -0800

    actually got it to work all the way through

--- updateAndReplace.sh
+++ updateAndReplace.sh
@@ -23,11 +23,26 @@ fail () {
     exit
 }
 
+check_clean () {
+    if [ -z "$(git status --porcelain)" ]; then 
+        return "true"
+    else 
+        return "false"
+    fi
+}
+
 update () {
     local folder=$1
     inform "about to update $folder"
     cd "$folder"
-    git pull origin master
+    local clean=check_clean
+    if [ clean == "true" ]
+    then
+        git pull origin master
+    else
+        pwd
+        #fail "skipped $folder due to unclean status"
+    fi
     cd ..
     success "updated $folder"
 }
@@ -46,11 +61,11 @@ update_scripts () {
 
 replace_scripts () {
     inform "about to replacing scripts"
-    for src in $(find "$TERMUX_HOME/" -maxdepth 3 -name '*.py')
+    for src in $(find "$TERMUX_HOME/workspace/" -maxdepth 3 -name '*.py')
     do
         inform "about to move $src"
-        dst="$home/.termux/tasker/$(basename "$src%.*")"
-        mv "$src $dst"
+        dst="$TERMUX_HOME/.termux/tasker/$(basename "${src%.*}")"
+        mv "$src" "$dst"
         success "moved $src to $dst"
     done
     success 'replaced all scripts'
