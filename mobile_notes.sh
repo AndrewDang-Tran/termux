f2c4eb0d99f6866f7bb14616a96b30ce1c1b8569
Andrew Dang-Tran <andrewdt1506@gmail.com>
2022-09-06 17:23:36 -0700

    add pull and push options to mobile notes

--- mobile_notes.sh
+++ mobile_notes.sh
@@ -2,8 +2,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-cd ~/storage/shared/obsidian-notes;
-git add .;
-git commit -m "Save mobile notes";
-git push origin main;
+if [ $1 == "push" ]
+    cd ~/storage/shared/obsidian-notes;
+    git add .;
+    git commit -m "Save mobile notes";
+    git push origin main;
+fi
+
+if [ $1 == "pull" ]
+    cd ~/storage/shared/obsidian-notes;
+    git pull origin main;
+fi
 
